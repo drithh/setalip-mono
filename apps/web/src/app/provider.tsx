6c30758939916077ff27525024beacc4c5a189c8
drithh <adrielalfeus@gmail.com>
2024-08-12 03:13:15 +0700

    whatsapp floating

--- apps/web/src/app/provider.tsx
+++ apps/web/src/app/provider.tsx
@@ -3,8 +3,10 @@
 import { TooltipProvider } from '@repo/ui/components/ui/tooltip';
 import { setDefaultOptions } from 'date-fns';
 import { id } from 'date-fns/locale';
+import { FloatingWhatsApp } from './_components/floating-whatsapp/floating-whatsapp';
 
 import { TRPCReactProvider } from '@/trpc/react';
+import { env } from '@repo/shared/env';
 setDefaultOptions({ locale: id });
 
 export default function Provider({
@@ -14,7 +16,19 @@ export default function Provider({
 }>) {
   return (
     <TRPCReactProvider>
-      <TooltipProvider>{children}</TooltipProvider>
+      <TooltipProvider>
+        {children}
+        <FloatingWhatsApp
+          phoneNumber="6281511673808"
+          accountName="Pilates Reform"
+          avatar={`/favicon-96x96.png`}
+          chatMessage="Halo, ada yang bisa kami bantu?"
+          allowEsc
+          allowClickAway
+          notification
+          notificationSound
+        />
+      </TooltipProvider>
     </TRPCReactProvider>
   );
 }
